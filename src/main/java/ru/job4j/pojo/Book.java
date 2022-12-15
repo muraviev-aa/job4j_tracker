0ff9fa5814f845966abc3f3b972f4aa720bc804c
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-12-15 08:01:41 +0300

    Массивы и модели.

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String name;
+    private int number;
+
+    public Book(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,37 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book = new Book("Stroll", 10);
+        Book book1 = new Book("Bounce", 15);
+        Book book2 = new Book("Horse", 12);
+        Book book3 = new Book("Clean code", 50);
+        Book[] bks = new Book[4];
+        bks[0] = book;
+        bks[1] = book1;
+        bks[2] = book2;
+        bks[3] = book3;
+        for (int index = 0; index < bks.length; index++) {
+            Book bk = bks[index];
+            System.out.println(bk.getName() + " - " + bk.getNumber());
+        }
+        System.out.println("-------------");
+        System.out.println("Replace [0] and [3].");
+        Book book4;
+        book4 = bks[0];
+        bks[0] = bks[3];
+        bks[3] = book4;
+        for (int index = 0; index < bks.length; index++) {
+            Book bk = bks[index];
+            System.out.println(bk.getName() + " - " + bk.getNumber());
+        }
+        System.out.println("-------------");
+        System.out.println("Output of books named Clean code");
+        for (int index = 0; index < bks.length; index++) {
+            Book bk = bks[index];
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + " - " + bk.getNumber());
+            }
+        }
+    }
+}
