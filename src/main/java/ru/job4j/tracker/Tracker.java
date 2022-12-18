43bb00babc1b969854d3f533fc8dd495237aefa2
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-12-18 10:54:20 +0300

    Метод замены заявки. Tracker.replace.

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != 1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -38,5 +31,26 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(rsl, counter);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            items[index] = item;
+            item.setId(id);
+        }
+        return result;
     }
 }
