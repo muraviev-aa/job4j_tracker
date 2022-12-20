b8376ec679a8acac7d57f43cb3a8ea3e1ff64e05
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-12-20 09:24:07 +0300

    Реализация класса StartUI. Вывод всех заявок. Added changes.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -18,8 +18,6 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select == 6) {
-                run = false;
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
@@ -30,6 +28,8 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
+            } else if (select == 6) {
+                run = false;
             }
         }
     }
