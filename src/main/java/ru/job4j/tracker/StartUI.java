cd8508de1b78ea24a47ffdb5ed48282dfea4d901
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-12-20 08:48:10 +0300

    Реализация класса StartUI. Добавление заявки.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,13 +11,17 @@ public class StartUI {
             showMenu();
             System.out.println("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select != 6) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
                 run = false;
             }
         }
-
     }
 
     private void showMenu() {
