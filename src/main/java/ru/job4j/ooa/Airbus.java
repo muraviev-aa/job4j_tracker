bebb24206968deb5fe7aeffccf830077fd1affa4
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-12-28 13:53:50 +0300

    final.

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,37 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+    private static final int COUNT_ENGINE_A380 = 4;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        System.out.println("Количество двигателей равно: "
+                + ("A380".equals(name) ? COUNT_ENGINE_A380 : COUNT_ENGINE));
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
