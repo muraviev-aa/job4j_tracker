3b3b009d14c45069bc119f245a288654c80221ad
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-01-19 12:26:55 +0300

    Идентичные тексты. Added changes.

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String fragment : text) {
             if (!check.contains(fragment)) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
