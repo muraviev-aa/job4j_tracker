c13b043f693c9cfaf2883e0103fef2bf2bc6a265
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-01-19 07:42:51 +0300

    Идентичные тексты.

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String part : origin) {
+            check.add(part);
+        }
+        for (String fragment : text) {
+            if (!check.contains(fragment)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
