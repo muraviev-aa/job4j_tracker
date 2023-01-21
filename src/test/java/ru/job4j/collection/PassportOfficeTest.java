5855ace6d31ddfa1b9c4ebf7fec8764faa062a30
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-01-21 15:23:42 +0300

    Паспорт и жители.

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenDupleNumberMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        Boolean expected = office.add(citizen);
+        assertThat(expected).isFalse();
+    }
+}
